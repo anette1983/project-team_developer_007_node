c3d9b5108e80fcf63f0c93541ef1382d3f072042
Maksym-Babich <maxus1042@gmail.com>
2023-07-15 19:55:53 +0300

    +

--- middlewares/cloudinaryUpload.js
+++ middlewares/cloudinaryUpload.js
@@ -1,24 +1,24 @@
 const cloudinary = require("../utils/cloudinary");
-const fs = require('fs')
+const fs = require("fs");
 
 const cloudinaryUpload = async (req, res, next) => {
-    const {fildname, path}= req.file
-    const {title} = req.body
-    await cloudinary.uploader.upload(
-      path,
-      { upload_preset: fildname, use_filename: true, public_id: `${title}` },
-      function (error, result) {
-        if (error) {
-          return res.status(500).json({
-            message: error.message,
-          });
-        }
-        fs.rm(req.file.path, { force: true }, () => {});
-        req.body.thumb = result.url;
+  const { fildname, path } = req.file;
+  const { title } = req.body;
+  await cloudinary.uploader.upload(
+    path,
+    { upload_preset: fildname, use_filename: true, public_id: `${title}` },
+    function (error, result) {
+      if (error) {
+        return res.status(500).json({
+          message: error.message,
+        });
       }
-    );
-    // console.log(uploadData);
-      next()
-}
+      fs.rm(req.file.path, { force: true }, () => {});
+      req.body.preview = result.url;
+    }
+  );
+  // console.log(uploadData);
+  next();
+};
 
 module.exports = cloudinaryUpload;
