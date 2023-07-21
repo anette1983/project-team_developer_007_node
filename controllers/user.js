14a6fac85c306839a3ec3a7676f557cf2698b747
Maksym Babich <maxus1042@gmail.com>
2023-07-21 10:28:56 +0300

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -52,10 +52,10 @@ const getShoppingList = async (req, res) => {
     },
   ]);
 
-  if (data.length === 0) {
-    res.json([])
-    // throw HttpError(404, "no ingredients found");
-  }
+  // if (data.length === 0) {
+  //   res.json([])
+  //   // throw HttpError(404, "no ingredients found");
+  // }
   const totalCount = Object.values(total[0]);
 
   res.json({ total: totalCount[0], list: [...data] });
