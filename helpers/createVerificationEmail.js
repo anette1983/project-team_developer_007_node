79bfc2d02b07c10f6d0c8cd164f26ceb7e89a783
Hanna <anna_kon@bigmir.net>
2023-09-15 01:31:32 +0300

    changed email colors to project colors

--- helpers/createVerificationEmail.js
+++ helpers/createVerificationEmail.js
@@ -81,7 +81,7 @@ const createVerificationEmail = (data) => {
     border-collapse: collapse !important;
   }
   a {
-    color: #1a82e2;
+    color: #8baa36;
   }
   img {
     height: auto;
@@ -135,7 +135,7 @@ const createVerificationEmail = (data) => {
         <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
           <tr>
             <td align="left" bgcolor="#ffffff" style="padding: 36px 24px 0; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; border-top: 3px solid #d4dadf;">
-              <h1 style="margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px;">Confirm Your Email Address</h1>
+              <h1 style="margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px; color: #8baa36; ">Confirm Your Email Address</h1>
             </td>
           </tr>
         </table>
@@ -174,7 +174,7 @@ const createVerificationEmail = (data) => {
                   <td align="center" bgcolor="#ffffff" style="padding: 12px;">
                     <table border="0" cellpadding="0" cellspacing="0">
                       <tr>
-                        <td align="center" bgcolor="#1a82e2" style="border-radius: 6px;">
+                        <td align="center" bgcolor="#8baa36" style="border-radius: 6px;">
                           <a href="${data.BASE_URL}/users/verify/${data.verificationTokenId}" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Verify your email</a>
                         </td>
                       </tr>
@@ -226,7 +226,7 @@ const createVerificationEmail = (data) => {
           <!-- start permission -->
           <tr>
             <td align="center" bgcolor="#e9ecef" style="padding: 12px 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px; color: #666;">
-              <p style="margin: 0;">You received this email because we received a request for registration for your account. If you didn't request registration at "So Yummy" you can safely delete this email.</p>
+              <p style="margin: 0;">You received this email because we received a registration request for your account at 'So Yummy.' If you didn't request this registration, you can safely delete this email.</p>
             </td>
           </tr>
           <!-- end permission -->
